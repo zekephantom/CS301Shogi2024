09a6b05fec7e3b39f65ed6e1765aea84e90d88c0
zekephantom <rafanan21@up.edu>
2024-09-19 19:40:39 -0700

    Modified external citations

--- app/src/main/java/edu/up/cs301shogi2024/MainActivity.java
+++ app/src/main/java/edu/up/cs301shogi2024/MainActivity.java
@@ -1,12 +1,17 @@
 package edu.up.cs301shogi2024;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button quit = findViewById(R.id.butQuit);
+        quit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                confirmExit();
+            }
+        });
+    }
+    private void confirmExit() {
+        // Show a confirmation dialog before exiting
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Quit App")
+                .setMessage("Are you sure you want to quit?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Close the app
+                        finishAffinity(); // Close all activities and exit
+                    }})
+                .setNegativeButton(android.R.string.no, null).show();
     }
 }
